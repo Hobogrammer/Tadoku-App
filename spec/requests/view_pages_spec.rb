872b954480f2db4640c2f4a6e566536d74cc2009
Andre Smith <lordsilent@gmail.com>
2012-06-23 15:47:32 -0500

    Added View pages controller

---
+++ spec/requests/view_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "ViewPages" do
+	
+	subject { page }  
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h3', text: 'Welcome to the Tadoku App!') }
+  end
+
+  describe "About page" do
+  		before { visit about_path }
+
+  		it { should have_selector('h3', text: 'About Tadoku') }
+  	end
+
+  	describe "Manual page" do
+  		before { visit manual_path }
+
+  		it { should have_selector('h3', text: 'Tadoku Manual') }
+  	end
+end
