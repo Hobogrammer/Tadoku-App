df5120f7ca7f2e135bf2d605bb71ee233c5ec765
Andre Smith <lordsilent@gmail.com>
2012-12-17 06:50:54 -0600

    removed some files

--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-	config.consumer_key = "EJTr4IUIYOuipLsbzh7BA"
-	config.consumer_secret = "iD5NNwclNlM6qdHg30dDdqqbuaiPgTwfDXpgTFFIg0"
-	config.oauth_token = "226853969-cRSRlwSSFvbTExn64EuuzwJsohAr74EovMtIJHWk"
-	config.oauth_token_secret = "5OVUfvRelID6LVMyOvp0qqNSVytxWzvbcsQq3GnzA"
-end
