a709572bd604c1063e82143a03bcf060c5331ba1
Andre Smith <lordsilent@gmail.com>
2014-03-16 18:12:42 -0700

    change match to get syntax

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,27 @@ TadokuApp::Application.routes.draw do
   resources :users do
     resources :updates, :rounds
   end
-  
+
 
   root to: 'view_pages#home'
 
-  match '/manual', to: 'view_pages#manual'
-  match '/about', to: 'view_pages#about'
-  match '/ranking', to: 'rounds#index'
-  
-  match '/signin' => redirect("/auth/twitter")
-  match '/signout', to: "sessions#destroy", via: :delete
+  get '/manual', to: 'view_pages#manual'
+  get '/about', to: 'view_pages#about'
+  get '/ranking', to: 'rounds#index'
 
-  match 'rounds/:round_id/:lang', to: "rounds#lang_show", :as => :lang
-  match 'rounds/:round_id' => 'rounds#show'
-  match '/ranking/0' => 'rounds#round0_show', :as => :zero
-  match 'rounds/:round_id/tier/:tier' => 'rounds#tier_show', :as => :tier
+  get '/signin' , to: redirect("/auth/twitter")
+  get '/signout', to: "sessions#destroy", via: :delete
 
-  match '/rounds/:round_id/users/:user_id' => 'users#old_show', :as => :old_user
-  match 'profile/:user_id' => 'users#profile'
+  get 'rounds/:round_id/:lang', to: "rounds#lang_show", :as => :lang
+  get 'rounds/:round_id' , to: 'rounds#show'
+  get '/ranking/0' , to: 'rounds#round0_show', :as => :zero
+  get 'rounds/:round_id/tier/:tier' , to: 'rounds#tier_show', :as => :tier
+
+  get '/rounds/:round_id/users/:user_id' , to: 'users#old_show', :as => :old_user
+  get 'profile/:user_id' , to: 'users#profile'
 
 
 
-  match "auth/twitter/callback" => "sessions#create"
+  get "auth/twitter/callback" , to: "sessions#create"
 end
 
