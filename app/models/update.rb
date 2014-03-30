d49925cb87f965332e3053a15be7d3dacbc86b4f
Andre Smith <lordsilent@gmail.com>
2014-03-30 16:09:27 -0700

    Move twitter initialization to updates model

--- app/models/update.rb
+++ app/models/update.rb
@@ -100,4 +100,14 @@ LANGUAGES = {
       update.save
     end
   end
+
+  def self.initialize_twitter
+     config = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["CONSUMER_KEY"]
+      config.consumer_secret = ENV["CONSUMER_SECRET"]
+      config.access_token = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
+    config
+  end
 end
