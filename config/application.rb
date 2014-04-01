d8a776ef5dde3efc7fe5ae9fc339bf8ba7151eb0
Andre Smith <lordsilent@gmail.com>
2014-03-31 22:46:38 -0700

    Remove whitelist and mass_assignment options

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module TadokuApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -50,7 +51,6 @@ module TadokuApp
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
