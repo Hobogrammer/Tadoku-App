9d59f6252b24a3e41051b974945e6752058be3af
Andre Smith <lordsilent@gmail.com>
2014-04-05 10:14:20 -0700

    Remove outdated schema information from user model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  provider   :string(255)
-#  uid        :string(255)
-#  name       :string(255)
-#  admin      :boolean         default(FALSE)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#
-
 class User < ActiveRecord::Base
   has_many :rounds, dependent: :destroy
   has_many :updates, dependent: :destroy
