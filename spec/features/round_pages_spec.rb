6c1dcc9634ea3708dc7abf676194a950c03c3749
Andre Smith <lordsilent@gmail.com>
2014-04-22 23:04:26 -0700

    Scaffolding out tests...sad excuse for a commit. Check on omniauth testing setup

--- spec/features/round_pages_spec.rb
+++ spec/features/round_pages_spec.rb
@@ -9,8 +9,24 @@ describe "RoundPages" do
       sign_in user
   end
 
+  describe "Ranking page" do
+    it "should contain all users" do
 
+    end
 
+    it "should link to users record for that round " do
+
+    end
+
+    it "should have the users page count" do
+
+    end
+  end
+
+  describe "Past ranking" do
+    it "should have the proper round link to the user profile" do
+    end
+  end
 end
 
 
