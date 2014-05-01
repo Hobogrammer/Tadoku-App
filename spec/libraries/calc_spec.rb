b5a0a2c2c4a23130694fae3f1f4a274f52dce2e5
Andre Smith <lordsilent@gmail.com>
2014-04-30 22:06:16 -0700

    Finish testing out the straight forward calculations. Need to test usermed_info function

--- spec/libraries/calc_spec.rb
+++ spec/libraries/calc_spec.rb
@@ -57,11 +57,102 @@ describe "Calc module" do
       score.should eq(74)
     end
 
-    describe "sentences" do
+    describe "Sentences" do
+      it "should return 5 points for 85 sentences read in jp, it, es, or en" do
+        score = Calc.sent_count(85, "jp")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 115 sentences in eo" do
+        score = Calc.sent_count(115, "eo")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 90 sentences in ko" do
+        score = Calc.sent_count(90, "ko")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 125 sentences in zh" do
+        score = Calc.sent_count(125, "zh")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 65 sentences in de" do
+        score = Calc.sent_count( 65,"de")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 80 sentences in fr" do
+        score = Calc.sent_count(80,"fr")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 120 sentences in ru" do
+        score = Calc.sent_count(120, "ru")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 150 sentences in nl" do
+        score = Calc.sent_count(150, "nl")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 155 sentences in af" do
+        score = Calc.sent_count(155, "af")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 40 sentences in ar" do
+        score = Calc.sent_count(40, "ar")
+
+        score.should eq(5)
+      end
+
+      it "should return 5 points for 165 sentences in all other languages" do
+        score = Calc.sent_count(165, "alien")
+
+        score.should eq(5)
+      end
     end
 
-    describe "repeats" do
+    describe "Repeats" do
+      it "should return 25 points for 50 book pages read twice" do
+        score = Calc.repeat(50,1)
+
+        score.should eq(25)
+      end
+
+      it "should return 12.5 for 50 book pages read three times" do
+        score = Calc.repeat(50,2)
+
+        score.should eq(12.5)
+      end
+
+      it "should return 6.25 for 50 book pages read four times" do
+        score = Calc.repeat(50, 3)
+
+        score.should eq(6.25)
+      end
+
+      it "should return 3.125 for 50 book pages read five times" do
+        score = Calc.repeat(50,4)
+
+        score.should eq(3.125)
+      end
     end
 
+    describe "User medium information" do
+      #fill in when users and rounds are properly being created via factory girl
+    end
   end
 end
