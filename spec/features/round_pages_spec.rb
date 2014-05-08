857f3c956de2a5e5c48f78de70856c983dc4d783
Andre Smith <lordsilent@gmail.com>
2014-05-07 23:30:12 -0700

    Try to fix round tests, no users showing up atm

--- spec/features/round_pages_spec.rb
+++ spec/features/round_pages_spec.rb
@@ -5,15 +5,20 @@ describe "RoundPages" do
 
   let(:user) { FactoryGirl.create(:user) }
 
-  before do
-    sign_in user
-  end
+  before(:all) { 30.times { FactoryGirl.create(:user) } }
+  after(:all) { User.delete_all }
 
   describe "Ranking page" do
+
+    before do
+      visit ranking_path
+    end
+
     it "should contain all users currently registered in the current round" do
       Round.where(round_id: ApplicationHelper::curr_round).each do |round|
         expect( page ).to have_selector('li', text: round.user.name)
       end
+      save_and_open_page
     end
 
     it "should link to users record for that round " do
