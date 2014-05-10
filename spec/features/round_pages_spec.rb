93f9bdc069c5e315e4581b161d46520ef0a6ec2e
Andre Smith <lordsilent@gmail.com>
2014-05-10 00:30:45 -0700

    Add standard tests for current round displaying properly.

--- spec/features/round_pages_spec.rb
+++ spec/features/round_pages_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe "RoundPages" do
   subject { page }
 
-  let(:user) { FactoryGirl.create(:user) }
+  before(:all) {
+    30.times { FactoryGirl.create(:user) }
+    30.times {FactoryGirl.create(:round) }
+  }
 
-  before(:all) { 30.times { FactoryGirl.create(:user) } }
-  after(:all) { User.delete_all }
+  after(:all) {
+    User.delete_all
+    Round.delete_all
+  }
 
   describe "Ranking page" do
 
@@ -16,23 +21,26 @@ describe "RoundPages" do
 
     it "should contain all users currently registered in the current round" do
       Round.where(round_id: ApplicationHelper::curr_round).each do |round|
-        expect( page ).to have_selector('li', text: round.user.name)
+        expect( page ).to have_selector('td', text: round.user.name)
       end
-      save_and_open_page
     end
 
     it "should link to users record for that round " do
-
+      Round.where(round_id: ApplicationHelper::curr_round).each do |round|
+        expect( page ).to have_link( "#{round.user.name}")
+      end
     end
 
     it "should have the users page count" do
-
+      Round.where(round_id: ApplicationHelper::curr_round).each do |round|
+        expect( page ).to have_selector('td', text: round.pcount)
+      end
     end
   end
 
   describe "Past ranking" do
     it "should have the proper round link to the user profile" do
-
+      #need to be able to generate rounds with not current round id
     end
   end
 end
