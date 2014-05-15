26c77afb3bf37e0918e440417c023ef29875874f
Andre Smith <lordsilent@gmail.com>
2014-05-15 00:03:16 -0700

    Create rounds associated with user object

--- spec/features/round_pages_spec.rb
+++ spec/features/round_pages_spec.rb
@@ -4,8 +4,13 @@ describe "RoundPages" do
   subject { page }
 
   before(:all) {
-    30.times { FactoryGirl.create(:user) }
-    30.times {FactoryGirl.create(:round) }
+    30.times  do |n|
+      user = FactoryGirl.create(:user)
+      round = user.rounds.new(round_id: ApplicationHelper::curr_round, lang1: 'jp',
+                                                        lang2: 'en', lang3:'zh', tier: 'Bronze', book:  "#{n}", manga: "#{n}",
+                                                        fgame: "#{n}", game: "#{n}", net: "#{n}", news: "#{n}", lyric:"#{n}",
+                                                        subs: "#{n}", nico: "#{n}", sent:"#{n}", pcount: "10#{n}")
+    end
   }
 
   after(:all) {
@@ -20,6 +25,8 @@ describe "RoundPages" do
     end
 
     it "should contain all users currently registered in the current round" do
+      round_count = Round.where(round_id: ApplicationHelper::curr_round).count
+      puts round_count
       Round.where(round_id: ApplicationHelper::curr_round).each do |round|
         expect( page ).to have_selector('td', text: round.user.name)
       end
