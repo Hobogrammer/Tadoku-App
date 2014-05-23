96017ec66a67b179d30386a7693b3a698d2d0821
Andre Smith <lordsilent@gmail.com>
2014-05-22 23:36:05 -0700

    Remove round factory and sequence from update factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :user do
-    sequence("id") { |n| "100#{n}"}
     sequence("name") { |n| "Person #{n}" }
     sequence("uid") { |n| "100#{n}"}
     provider "twitter"
@@ -12,29 +11,8 @@ FactoryGirl.define do
       end
     end
 
-    factory :round do
-      round_id ApplicationHelper::curr_round
-      sequence("user_id") { |n| "100#{n}"}
-      sequence("book") { |n| }
-      sequence("manga") { |n| "#{n}" }
-      sequence("fgame") { |n| "#{n}" }
-      sequence("game") { |n| "#{n}" }
-      sequence("net") { |n| "#{n}" }
-      sequence("news") { |n| "#{n}" }
-      sequence("lyric") { |n| "#{n}" }
-      sequence("subs") { |n| "#{n}" }
-      sequence("nico") { |n| "#{n}" }
-      sequence("sent") { |n| "#{n}" }
-      sequence("pcount") { |n| "10#{n}"}
-      goal 0
-      lang1 "jp"
-      lang2 "en"
-      lang3 "zh"
-      tier "Bronze"
-  end
-
   factory :update do
-    sequence("user_id") { |n| "100#{n}"}
+    user
     medium "book"
     sequence("newread") { |n| "#{n}"}
     lang "jp"
