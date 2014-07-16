cfe00a54748b7e3a3af551a91e277c0f02f9e2eb
Andre Smith <lordsilent@gmail.com>
2014-07-15 23:29:49 -0700

    Indentation

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -245,24 +245,24 @@ end
   end
 
     def self.rollback(user,round) #originally intended for use in both the bot and app, move to bot since application cannot use
-    del_update = user.updates.where(:round_id => round).last
+      del_update = user.updates.where(:round_id => round).last
 
-    if !del_update.present?
-       false
-    else
-      unread = del_update.raw.to_f
-      unmed = del_update.medium.to_s
-      rev_total = del_update.recpage.to_f
-      usr_round = user.rounds.find_by_round_id(round)
+      if !del_update.present?
+         false
+      else
+        unread = del_update.raw.to_f
+        unmed = del_update.medium.to_s
+        rev_total = del_update.recpage.to_f
+        usr_round = user.rounds.find_by_round_id(round)
 
-      old_read = usr_round.send(unmed).to_f
-      rev_read = old_read.to_f - unread.to_f
-      usr_round.update_attributes(unmed.to_sym => rev_read)
+        old_read = usr_round.send(unmed).to_f
+        rev_read = old_read.to_f - unread.to_f
+        usr_round.update_attributes(unmed.to_sym => rev_read)
 
-      usr_round.update_attributes(:pcount => rev_total)
-      del_update.destroy
+        usr_round.update_attributes(:pcount => rev_total)
+        del_update.destroy
 
-      rev_total
-    end
+        rev_total
+      end
   end
 end
