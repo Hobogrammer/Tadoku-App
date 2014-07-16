0db43109850738a345481316f38acbef20e266b4
Andre Smith <lordsilent@gmail.com>
2014-07-15 22:46:41 -0700

    Remove a function that was mostly duplication

--- lib/data_restore.rb
+++ lib/data_restore.rb
@@ -1,9 +1,9 @@
 class DataRestore
 
-  def csv_backup_restore
+  def csv_backup_restore(round_id)
       client = Update.initialize_twitter
 
-      File.readlines("oldDB/tadoku04.csv").each do |row|
+      File.readlines("oldDB/#{round_id}.csv").each do |row|
         row = row.gsub('"', "")
         row = row.split(/;/)
 
@@ -34,54 +34,16 @@ class DataRestore
 
         puts "making round for user"
         usr = User.find_by_name(pre_usr)
-        rest_round = usr.rounds.new(:round_id => "201110")
+        rest_round = usr.rounds.new(:round_id => round_id)
         rest_round.save
 
-        usr.rounds.find_by_round_id("201110").update_attributes(:lang1 => row[13] , :lang2 => row[14] , :lang3 => row[15] ,
-          :book => row[2], :manga => row[3], :net => row[4], :fgame => row[5], :game => row[6], :lyric => row[7], :subs => row[8],
-          :news => row[9], :sent => row[10], :nico => row[11], :pcount => row[12])
-
-      end
-    end
-
-    def restore_round_zero #is this the same as above?
-      client = Update.initialize_twitter
-
-      File.readlines("oldDB/ranking.csv").each do |row|
-        row = row.gsub('"', "")
-        row = row.split(/;/)
-
-        pre_usr = row[1].to_s
-        usr = User.find_by_name(pre_usr)
-
-        uid = rand(100000)
-
-        if usr.nil?
-          begin
-            twi_usr = client.user(pre_usr)
-            puts "called to twitter"
-          rescue Twitter::Error
-            puts "Twitter error"
-            usr = User.new(:name => pre_usr, :provider => "twitter", :uid => uid, :time_zone => "UTC")
-            usr.save
-          else
-            puts "call twitter call OK"
-            tz = twi_usr.time_zone
-
-            if tz.nil?
-              tz = "UTC" # Wouldn't let me directly assign .time_zone
-            end
-            new_usr = User.new(:name => pre_usr, :provider => "twitter", :uid => twi_usr.id, :time_zone => tz)
-            new_usr.save
-          end
+        if round_id = 201008
+          usr.rounds.find_by_round_id(round_id).update_attributes(:lang1 => "jp", :pcount => row[2])
+        else
+          usr.rounds.find_by_round_id(round_id).update_attributes(:lang1 => row[13] , :lang2 => row[14] , :lang3 => row[15] ,
+            :book => row[2], :manga => row[3], :net => row[4], :fgame => row[5], :game => row[6], :lyric => row[7], :subs => row[8],
+            :news => row[9], :sent => row[10], :nico => row[11], :pcount => row[12])
         end
-
-        puts "making round for user"
-        usr = User.find_by_name(pre_usr)
-        rest_round = usr.rounds.new(:round_id => "201008")
-        rest_round.save
-
-        usr.rounds.find_by_round_id("201008").update_attributes(:lang1 => "jp", :pcount => row[2])
       end
     end
 
@@ -104,7 +66,7 @@ class DataRestore
           nico_tot += round.nico
          total += round.pcount
         end
-        over_round = user.rounds.new(:round_id => 000001, :book => book_tot, :manga=> manga_tot, :net => net_tot,
+        over_round = user.rounds.new(:round_id => 01, :book => book_tot, :manga=> manga_tot, :net => net_tot,
           :game => game_tot, :fgame => fgame_tot, :news => news_tot, :lyric => lyric_tot, :subs => subs_tot,
           :sent => sent_tot, :nico => nico_tot, :pcount => total)
         over_round.save
