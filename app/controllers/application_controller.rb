33fa767019a422a1b10b276d78aedd301c8a205a
Andre Smith <lordsilent@gmail.com>
2014-07-21 22:46:37 -0700

    Finish automagical filling of round hash for menu. Next step: make the menu automatic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,20 @@ class ApplicationController < ActionController::Base
   def previous_rounds
     round_list = ApplicationHelper.prev_rounds
     year_list = years
-    round_hash = seed_round_hash(year_list)
+    @ordered_rounds = seed_round_hash(year_list)
 
     round_list.each do |round|
       year_list.each do |year|
-        round_hash["#{year}"] << round if round.match(/"#{year}".+/)
+        @ordered_rounds["#{year}"] << round if round.to_s.match(/#{year}.+/)
       end
     end
-
-    round_hash do { |x| x.sort }
-
-    @ordered_rounds = {"2011" => rounds_2011.sort, "2012" => rounds_2012.sort, "2013" => rounds_2013.sort, "2014" => old_rounds.sort }
-    #TODO: make this more automatic
+    @ordered_rounds.each { |k,v| @ordered_rounds[k] = v.sort }
   end
 
   def years
-    year_list = []
     start_year = 2010
     current_year = Date.today.year
-    while start_year <= current_year
-      year_list << start_year
-      start_year += 1
-    end
-    year_list
+    year_list = (start_year..current_year).to_a
   end
 
   def seed_round_hash(year_list)
