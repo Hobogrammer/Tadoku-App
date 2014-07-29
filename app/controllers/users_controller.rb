278d0d3fed1f8b7cf55082575de678eb302fcc15
Andre Smith <lordsilent@gmail.com>
2014-07-28 22:58:44 -0700

    Remove old_show and make show more flexible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,17 @@ class UsersController < ApplicationController
   before_filter :admin_user, only: [:destroy, :edit]
 
   def show
-    @user = User.find(params[:id])
-    @update = ApplicationHelper.build_update(signed_in?)
-
-    if !@user.rounds.find_by_round_id(ApplicationHelper.curr_round).nil?
-      @round_stats = Calc.usermed_info(@user,ApplicationHelper.curr_round)
-      @round = @user.rounds.find_by_round_id(ApplicationHelper.curr_round)
-      @avg = Calc.month_reading_average(@user, ApplicationHelper.curr_round, @round.pcount)
-      @updates = @user.updates.where(:round_id => ApplicationHelper.curr_round).order('created_at DESC').limit(10)
-    else
-      flash[:error] = "This user is not registered for the current round. For past round records please  access the old rankings."
-      redirect_to ranking_path
-    end
-  end
-
-  def old_show
     @user = User.find(params[:user_id])
-
     @round_id = params[:round_id]
     @update = ApplicationHelper.build_update(signed_in?)
 
-    if !@user.rounds.find_by_round_id(params[:round_id]).nil?
-      @round_stats = Calc.usermed_info(@user,params[:round_id])
-      @round = @user.rounds.find_by_round_id(params[:round_id])
-      @avg = Calc.month_reading_average(@user, ApplicationHelper.curr_round, @round.pcount)
-      @updates = @user.updates.where(:round_id => params[:round_id]).order('created_at DESC').limit(10)
+    if !@user.rounds.find_by_round_id(@round_id).nil?
+      @round_stats = Calc.usermed_info(@user,@round_id)
+      @round = @user.rounds.find_by_round_id(@round_id)
+      @avg = Calc.month_reading_average(@user, @round_id, @round.pcount)
+      @updates = @user.updates.where(:round_id => @round_id).order('created_at DESC').limit(10)
     else
-      flash[:error] = "This user is not registered for this round."
+      flash[:error] = "This user is not registered for the current round. For past round records please  access the old rankings."
       redirect_to ranking_path
     end
   end
