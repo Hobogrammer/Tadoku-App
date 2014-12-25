f5dbea42c02717fd60cf6bf30f93edb6807aeca3
Andre Smith <lordsilent@gmail.com>
2014-12-25 02:09:47 -0800

    Moved method into model

--- app/models/round.rb
+++ app/models/round.rb
@@ -79,7 +79,16 @@ class Round < ActiveRecord::Base
   end
 
   def self.previous_rounds
-    
+    round_list = ApplicationHelper.prev_rounds
+    year_list = years
+    @ordered_rounds = seed_round_hash(year_list)
+
+    round_list.each do |round|
+      year_list.each do |year|
+        @ordered_rounds["#{year}"] << round if round.to_s.match(/#{year}.+/)
+      end
+    end
+    @ordered_rounds.each { |k,v| @ordered_rounds[k] = v.sort }
   end
 
   private
@@ -87,5 +96,13 @@ class Round < ActiveRecord::Base
   def years
     current_year = Date.today.year
     year_list = (2010..current_year).to_a
+  end
+
+  def seed_round_hash(year_list)
+    round_hash = {}
+    year_list.each do |year|
+      round_hash["#{year}"] = []
+    end
+    round_hash
   end
 end
