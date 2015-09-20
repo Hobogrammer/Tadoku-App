d38cec8ff6b4790e6e33710b0dce429dcd630afa
Hobogrammer <andrepsmithjr@gmail.com>
2015-09-20 16:12:59 -0700

    Postgres install
    Postgres install

--- db/schema.rb
+++ db/schema.rb
@@ -9,70 +9,73 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613030900) do
+ActiveRecord::Schema.define(version: 20130613030900) do
 
-  create_table "bots", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bots", force: true do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "rounds", :force => true do |t|
+  create_table "rounds", force: true do |t|
     t.integer  "user_id"
     t.integer  "round_id"
-    t.decimal  "book",       :default => 0.0
-    t.decimal  "manga",      :default => 0.0
-    t.decimal  "net",        :default => 0.0
-    t.decimal  "fgame",      :default => 0.0
-    t.decimal  "game",       :default => 0.0
-    t.decimal  "lyric",      :default => 0.0
-    t.decimal  "subs",       :default => 0.0
-    t.decimal  "news",       :default => 0.0
-    t.decimal  "sent",       :default => 0.0
-    t.decimal  "nico",       :default => 0.0
-    t.decimal  "pcount",     :default => 0.0
+    t.decimal  "book",       default: 0.0
+    t.decimal  "manga",      default: 0.0
+    t.decimal  "net",        default: 0.0
+    t.decimal  "fgame",      default: 0.0
+    t.decimal  "game",       default: 0.0
+    t.decimal  "lyric",      default: 0.0
+    t.decimal  "subs",       default: 0.0
+    t.decimal  "news",       default: 0.0
+    t.decimal  "sent",       default: 0.0
+    t.decimal  "nico",       default: 0.0
+    t.decimal  "pcount",     default: 0.0
     t.integer  "goal"
-    t.boolean  "gmet",       :default => false
-    t.string   "lang1",      :default => ""
-    t.string   "lang2",      :default => ""
-    t.string   "lang3",      :default => ""
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.string   "tier",       :default => "0"
+    t.boolean  "gmet",       default: false
+    t.string   "lang1",      default: ""
+    t.string   "lang2",      default: ""
+    t.string   "lang3",      default: ""
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "tier",       default: "0"
   end
 
-  create_table "tiers", :force => true do |t|
+  create_table "tiers", force: true do |t|
     t.string  "uid"
     t.string  "prev_tier"
     t.string  "curr_tier"
     t.boolean "pending"
   end
 
-  create_table "updates", :force => true do |t|
+  create_table "updates", force: true do |t|
     t.integer  "user_id"
     t.integer  "round_id"
-    t.decimal  "newread",                 :default => 0.0
+    t.decimal  "newread",                 default: 0.0
     t.string   "medium"
     t.string   "lang"
-    t.decimal  "recpage",                 :default => 0.0
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
-    t.integer  "repeat",                  :default => 0
-    t.boolean  "dr",                      :default => false
-    t.decimal  "raw",                     :default => 0.0
+    t.decimal  "recpage",                 default: 0.0
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.integer  "repeat",                  default: 0
+    t.boolean  "dr",                      default: false
+    t.decimal  "raw",                     default: 0.0
     t.datetime "created_at_in_user_time"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.boolean  "admin",      :default => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "admin",      default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "time_zone"
-    t.string   "avatar",     :default => ""
+    t.string   "avatar",     default: ""
   end
 
 end
