5001995f950d199093a176033e0a3f63932eb1ca
Hobogrammer <andrepsmithjr@gmail.com>
2017-01-29 14:41:53 -0800

    Start user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(user_params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:id, :profile_img, :name, :timezone)
+    end
+
 end
